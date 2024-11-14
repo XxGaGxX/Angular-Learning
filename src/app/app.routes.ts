1ab0fdcfe6b8877c52a49760a02d3c1b9876d0f1
diego.vagnini <diego.vagnini.stud@ispascalcomandini.it>
2024-11-14 11:24:08 +0100

    Nuovo progetto (mandato da tonetti)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../componenti/home/home.component';
+import { ChisiamoComponent } from '../componenti/chisiamo/chisiamo.component';
+import { CatalogoComponent } from '../componenti/catalogo/catalogo.component';
+import { ContattiComponent } from '../componenti/contatti/contatti.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
+    },
+    {
+        path: "chisiamo",
+        component: ChisiamoComponent
+    },
+    {
+        path: "catalogo",
+        component: CatalogoComponent
+    },
+    {
+        path: "contatti",
+        component: ContattiComponent
+    }
+];
